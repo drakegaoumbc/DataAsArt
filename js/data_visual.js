24bafe0c0c5fff24ce8a1188b6fe32cd14e53fa6
drakegaoumbc <chengao1@umbc.edu>
2017-11-29 22:05:35 -0500

    fond size

--- js/data_visual.js
+++ js/data_visual.js
@@ -1,4 +1,4 @@
-$(function() {  
+$(function() {
 
   function D3_TreeMap() {
   'use strict';
@@ -8,7 +8,7 @@ $(function() {
     const height = 720 - margin.top - margin.bottom;*/
     const color = d3.scaleOrdinal()
               .range(["#EF7087","#A724E8","#8CBAD1","#DDA335","#D981D5","#82CE8C","#839BE6","#C6D445"]);
-        
+
     var margin = {top: 40, right: 10, bottom: 10, left: 10};
         //width = window.innerWidth - margin.left - margin.right,
         //height = 500 - margin.top - margin.bottom;
@@ -35,7 +35,7 @@ $(function() {
 
       //const root = d3.hierarchy(data, (d) => d.children.sum(d) => d.value);
       const root = d3.hierarchy(data, (d) => d.children)
-      .sum((d) => d.value);
+      .sum((d) => (d.value/914763));
       const treeMap = treemap(root);
 
       const node = div.datum(root).selectAll(".node")
@@ -99,14 +99,14 @@ $(function() {
 
     var svg = d3.select("div#barchart").append("svg")
                 .attr("width", width + margin.left + margin.right)
-                .attr("height", height + margin.top + margin.bottom)
+                .attr("height", height + margin.top + margin.bottom + 200)
                 .append("g")
                 .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    
+
     d3.csv("/DataAsArt/assets/csvfiles/data.csv", function(error, data) {
       if(error) throw error;
-      
+
       data.forEach(function(d) {
         d.value = +d.value;
       });
@@ -115,6 +115,7 @@ $(function() {
       y.domain([0, d3.max(data, function(d) { return (d.value+0.1); })]);
 
       // append retangles
+
       svg.selectAll(".bar").data(data)
          .enter().append("rect")
          .attr("class", "bar")
@@ -131,16 +132,16 @@ $(function() {
          .attr("class", "bar--x")
          .attr("transform", "translate(0," + height + ")")
          .call(d3.axisBottom(x).ticks(10))
-        .selectAll("text")  
+        .selectAll("text")
           .style("text-anchor", "begin")
-          .attr("dx", "6em")
+          .attr("dx", "-5em")
           .attr("dy", "-2em")
           .attr("transform", "rotate(-65)");
 
       svg.append("g")
          .call(d3.axisLeft(y).ticks(10, "%"))
          .append("text")
-         .attr("y", 6)
+         .attr("y", 35)
          .attr("text-anchor", "end")
          .text("Percentage");
     });
@@ -152,65 +153,6 @@ $(function() {
   var dataAge = [77.79, 77.83, 77.85, 77.97, 78.07, 78.43, 78.63, 79.01, 79.16, 79.25, 79.36, 79.85, 80];
 
   D3_TreeMap();
-
-  var years = [];
-  for(var i = 2000; i < 2016; i++) {
-    years.push(i);
-  }  
-
-  $( "#slider-range-max" ).slider({
-      range: "max",
-      min: 2000,
-      max: 2015,
-      value: 2000,
-      slide: function( event, ui ) {
-        $( "#amount" ).val(ui.value);
-        switchImg(ui.value);
-        
-      }
-  });
-  $("#amount").val($("#slider-range-max").slider("value"));
-
-  console.log(years);
-  var switchImg = function(year) {
-    var imgSrc = $("img");
-    var age = $('#age');
-    console.log(age);
-    for(var i = 2000; i < 2016; i++) {
-      if(i === +year) {
-        if(i === 2000) {
-          imgSrc.prop('src', 'images/76.jpg');
-          age.text('76');
-        } else if(i >= 2001 && i <= 2004) {
-          imgSrc.prop('src', 'images/77.jpg');
-          age.text('77');
-        } else if(i >= 2005 && i <= 2010) {
-          imgSrc.prop('src', 'images/78.jpg');
-          age.text('78');
-        } else if(i >= 2011 && i <= 2014) {
-          imgSrc.prop('src', 'images/79.jpg');
-          age.text('79');
-        } else if(i === 2015) {
-          imgSrc.prop('src', 'images/80.jpg');
-          age.text('80');
-        }
-      } 
-    }
-  }
-
-  // sliding effect
-  $(".slide").on('click', function(e) {
-    var $this = $(this);
-    var active = $this.parents('ul#nav-ul').find('li.active');
-    active.removeClass('active');
-    $this.addClass('active');
-
-    var linkHref = $this.find('a').attr('href');
-    $('html, body').animate({
-      scrollTop: $(linkHref).offset().top
-    });
-    e.preventDefault();
-  });
 
 });
 
@@ -270,47 +212,63 @@ d3.json("/DataAsArt/assets/json/test.json", function(error, data) {
   });
 });
 
+var years = [];
+for(var i = 2000; i < 2016; i++) {
+  years.push(i);
+}
 
-White female,Diseases of heart,318668
-White female,Malignant neoplasms,169974
-White female,Cerebrovascular diseases,88639
-White female,Unintentional injuries,27159
-White female,Pneumonia and influenza,24559
-White female,Diabetes mellitus,16743
-White female,Chronic obstructive pulmonary diseases,16398
-White female,Homicide,10
-Black or African American female,Diseases of heart,35079
-Black or African American female,Malignant neoplasms,19176
-Black or African American female,Cerebrovascular diseases,88639
-Black or African American female,Unintentional injuries,3779
-Black or African American female,Pneumonia and influenza,2262
-Black or African American female,Diabetes mellitus,3534
-Black or African American female,Chronic obstructive pulmonary diseases,0
-Black or African American female,Homicide,1898
-American Indian or Alaska Native female,Diseases of heart,577
-American Indian or Alaska Native female,Malignant neoplasms,362
-American Indian or Alaska Native female,Cerebrovascular diseases,159
-American Indian or Alaska Native female,Unintentional injuries,344
-American Indian or Alaska Native female,Pneumonia and influenza,109
-American Indian or Alaska Native female,Diabetes mellitus,124
-American Indian or Alaska Native female,Chronic obstructive pulmonary diseases,0
-American Indian or Alaska Native female,Homicide,55
-Asian or Pacific Islander female,Diseases of heart,1091
-Asian or Pacific Islander female,Malignant neoplasms,1037
-Asian or Pacific Islander female,Cerebrovascular diseases,507
-Asian or Pacific Islander female,Unintentional injuries,254
-Asian or Pacific Islander female,Pneumonia and influenza,115
-Asian or Pacific Islander female,Diabetes mellitus,124
-Asian or Pacific Islander female,Chronic obstructive pulmonary diseases,124
-Asian or Pacific Islander female,Homicide,60
+$( "#slider-range-max" ).slider({
+    range: "max",
+    min: 2000,
+    max: 2015,
+    value: 2000,
+    slide: function( event, ui ) {
+      $( "#amount" ).val(ui.value);
+      switchImg(ui.value);
 
-Diseases of heart,355395
-Malignant neoplasms,190549
-Cerebrovascular diseases,100246
-Unintentional injuries,31526
-Pneumonia and influenza,27045
-Diabetes mellitus,20525
-Chronic obstructive pulmonary diseases,16522
-Homicide,2023
+    }
+});
+$("#amount").val($("#slider-range-max").slider("value"));
+
+console.log(years);
+var switchImg = function(year) {
+  var imgSrc = $("img");
+  var age = $('#age');
+  console.log(age);
+  for(var i = 2000; i < 2016; i++) {
+    if(i === +year) {
+      if(i === 2000) {
+        imgSrc.prop('src', 'images/76.jpg');
+        age.text('76');
+      } else if(i >= 2001 && i <= 2004) {
+        imgSrc.prop('src', 'images/77.jpg');
+        age.text('77');
+      } else if(i >= 2005 && i <= 2010) {
+        imgSrc.prop('src', 'images/78.jpg');
+        age.text('78');
+      } else if(i >= 2011 && i <= 2014) {
+        imgSrc.prop('src', 'images/79.jpg');
+        age.text('79');
+      } else if(i === 2015) {
+        imgSrc.prop('src', 'images/80.jpg');
+        age.text('80');
+      }
+    }
+  }
+}
+
+// sliding effect
+$(".slide").on('click', function(e) {
+  var $this = $(this);
+  var active = $this.parents('ul#nav-ul').find('li.active');
+  active.removeClass('active');
+  $this.addClass('active');
+
+  var linkHref = $this.find('a').attr('href');
+  $('html, body').animate({
+    scrollTop: $(linkHref).offset().top
+  });
+  e.preventDefault();
+});
 
 */
